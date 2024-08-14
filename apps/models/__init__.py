6fd8dd48d2f9808cbe4800f1a7466a2d1fc9fc7a
shokhruzaibodova <shokhruzaibodova@gmail.com>
2024-08-14 13:55:13 +0500

    news model

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,4 +1,4 @@
-from apps.models.avrertisement import Adv, AdvImage, AdvSubcategoryField
+from apps.models.advertisement import Adv, AdvImage, AdvSubcategoryField
 from apps.models.categories import Category, SubCategory, Field
 from apps.models.location import Region, District
 from apps.models.mark_models import MarksModel
